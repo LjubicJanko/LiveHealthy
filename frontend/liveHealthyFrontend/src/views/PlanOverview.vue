72ff207fdd1f4c06bf185ed25d88cb3b54c36362
Janko <ljubicjanko1@gmail.com>
2020-06-01 20:50:10 +0200

    prepare for course change

--- frontend/liveHealthyFrontend/src/views/PlanOverview.vue
+++ frontend/liveHealthyFrontend/src/views/PlanOverview.vue
@@ -5,14 +5,41 @@
       <training-plan></training-plan>
     </div>
     <div v-else class="wrapper">
-      <v-btn id="createPlanBtn" @click="createPlan()" x-large>
-        <v-icon>mdi-food-fork-drink</v-icon>
-        <v-divider class="mx-4" vertical></v-divider>
-        <h2>Create nutrition and training plan</h2>
-        <v-divider class="mx-4" vertical></v-divider>
-        <v-icon>mdi-weight-lifter</v-icon>
-      </v-btn>
+      <v-dialog v-model="dialog" persistent max-width="290">
+        <template v-slot:activator="{ on }">
+          <!-- <v-btn color="primary" dark v-on="on">Open Dialog</v-btn> -->
+          <v-btn id="createPlanBtn" x-large v-on="on">
+            <v-icon>mdi-food-fork-drink</v-icon>
+            <v-divider class="mx-4" vertical></v-divider>
+            <h2>Create nutrition and training plan</h2>
+            <v-divider class="mx-4" vertical></v-divider>
+            <v-icon>mdi-weight-lifter</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title class="headline">Any decease we need to know about?</v-card-title>
+          <v-card-text>
+            <v-checkbox v-model="illnesses" label="HEADACHES" value="HEADACHES"></v-checkbox>
+            <v-checkbox v-model="illnesses" label="HEART DISEASE" value="HEART_DISEASE"></v-checkbox>
+            <v-checkbox v-model="illnesses" label="ARM INJURY" value="ARM_INJURY"></v-checkbox>
+            <v-checkbox v-model="illnesses" label="LEG INJURY" value="LEG_INJURY"></v-checkbox>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+            <v-btn color="green darken-1" text @click="createPlan()">Create</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="loading" fullscreen full-width>
+        <v-container fluid fill-height style="background-color: rgba(255, 255, 255, 0.5);">
+          <v-layout justify-center align-center>
+            <v-progress-circular size="100" width="15" indeterminate color="primary"></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
     </div>
+
     <v-snackbar v-model="snackbar.show" :timeout="5000" :color="snackbar.color" :top="true">
       {{snackbar.msg}}
       <v-btn dark @click="snackbar.show = false">Close</v-btn>
@@ -39,6 +66,9 @@ export default {
     planExistis: false,
     user: {},
     nutritionPlan: {},
+    dialog: false,
+    illnesses: [],
+    loading: false,
     snackbar: {
       show: false,
       color: "",
@@ -51,22 +81,33 @@ export default {
   methods: {
     createPlan() {
       if (store.state.userLoggedIn) {
+        this.dialog = false;
+        this.loading = true;
         let createPlanInfoDto = {
           forbiddenFoodDto: {
             forbiddenFood: ["Cheese- brie"]
           },
           illnessesDto: {
-            illnesses: ["HEART_DISEASE"]
+            illnesses: this.illnesses
+            // illnesses: ["HEART_DISEASE"]
           }
         };
 
         if (this.user.bodyType != null) {
-          PlanService.create(store.state.userId, createPlanInfoDto).then(response => {
-            console.log(response.data);
-            this.planExistis = true;
-          });
+          PlanService.create(store.state.userId, createPlanInfoDto).then(
+            response => {
+              console.log(response.data);
+              this.planExistis = true;
+              this.loading = false;
+              window.location.reload();
+            }
+          );
         } else {
-          this.showSnackbar("You can not create plan before determinating body type!", "error");
+          this.loading = false;
+          this.showSnackbar(
+            "You can not create plan before determinating body type!",
+            "error"
+          );
           this.$router.push("/body-type");
         }
       }
@@ -74,7 +115,7 @@ export default {
     getUserPlan() {
       UserService.getUser(this.$route.params.userId).then(response => {
         this.user = response.data;
-        if(this.user.nutritionPlan == null) {
+        if (this.user.nutritionPlan == null) {
           this.planExistis = false;
         } else {
           this.planExistis = true;
@@ -87,7 +128,7 @@ export default {
       this.snackbar.color = color;
       this.snackbar.msg = message;
       this.snackbar.show = true;
-    },
+    }
   },
   created() {}
 };
