69e82b1fd8710a4e08a86d8ea195d5f27025129e
Janko <ljubicjanko1@gmail.com>
2020-07-01 13:07:59 +0200

    tests found bug, fixed it

--- backend/live-healthy-app/src/test/java/live/healthy/service/nutritionPlan/CreatePlanTest.java
+++ backend/live-healthy-app/src/test/java/live/healthy/service/nutritionPlan/CreatePlanTest.java
@@ -70,4 +70,72 @@ public class CreatePlanTest {
         assertEquals(2, numOfRulesFired);
         assertEquals("EXTREME_WEIGHT_GAIN", planDto.getGoal());
     }
+
+
+    @Test
+    public void extreemeWeightGainWithMoreFatsTest() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("live.healthy.kjar", "live-healthy-kjar", "0.0.1-SNAPSHOT"));
+
+        KieSession kieSession = kContainer.newKieSession("creatingPlan");
+
+        User user = new User();
+        user.setWeight(50.0);
+        user.setIdealBodyWeight(87.0);
+        user.setStartingBmi(20.0);
+        user.setSex(false);
+        user.setAge(18);
+        user.setStartingBfp(7.5);
+
+        BodyType bodyType = new BodyType();
+        bodyType.setBodyTypeEnum(BodyTypeEnum.ECTOMORPH);
+
+        PlanDto planDto = new PlanDto();
+        planDto.setGoal("");
+
+        kieSession.setGlobal("bodyType", BodyTypeEnum.ECTOMORPH.name());
+
+        kieSession.insert(user);
+        kieSession.insert(planDto);
+
+        int numOfRulesFired = kieSession.fireAllRules();
+
+        assertEquals(3, numOfRulesFired);
+        assertEquals("EXTREME_WEIGHT_GAIN", planDto.getGoal());
+    }
+
+//
+//    @Test
+//    public void extreemeWeightLossNoFatTest() {
+//        KieServices ks = KieServices.Factory.get();
+//        KieContainer kContainer = ks
+//                .newKieContainer(ks.newReleaseId("live.healthy.kjar", "live-healthy-kjar", "0.0.1-SNAPSHOT"));
+//
+//        KieSession kieSession = kContainer.newKieSession("creatingPlan");
+//
+//        User user = new User();
+//        user.setWeight(87.0);
+//        user.setIdealBodyWeight(50.0);
+//        user.setStartingBmi(29.0);
+//        user.setSex(true);
+//        user.setAge(18);
+//        user.setStartingBfp(9.0);
+//
+//        BodyType bodyType = new BodyType();
+//        bodyType.setBodyTypeEnum(BodyTypeEnum.ENDOMORPH);
+//
+//        PlanDto planDto = new PlanDto();
+//        planDto.setGoal("");
+//
+//        kieSession.setGlobal("bodyType", BodyTypeEnum.ENDOMORPH.name());
+//
+//        kieSession.insert(user);
+//        kieSession.insert(planDto);
+//
+//        int numOfRulesFired = kieSession.fireAllRules();
+//
+//        assertEquals(3, numOfRulesFired);
+//        assertEquals("EXTREME_WEIGHT_LOSS", planDto.getGoal());
+//    }
 }
